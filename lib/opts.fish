4f1bc0d1ae83721c2e9ba8ff3e67662efce1a443
scott-linder <scott.b.linder@wmich.edu>
2015-10-14 11:42:48 -0400

    Update lib/opts.fish to properly handle quoting

--- lib/opts.fish
+++ lib/opts.fish
@@ -8,35 +8,30 @@ function opts_find_sep -d 'Get index of -- separator from opts'
     echo $idx
 end
 
-function opts_flags -d 'Get the flag arguments from opts'
-    set -l sep $argv[1]
-    set -l opts $argv[2..-1]
-    if test $sep != 1
-        echo $opts[1..(math "$sep - 1")] | tr ' ' \n
-    end
-end
-
-function opts_rest -d 'Get the rest (positional) arguments from opts'
-    set -l sep $argv[1]
-    set -l opts $argv[2..-1]
-    if test $sep != (count $opts)
-        echo $opts[(math "$sep + 1")..-1] | tr ' ' \n
-    end
-end
-
 function opts_getopts -d 'Parse commandline options from args'
     set -l fmt $argv[1]
-    set -l args $argv[2]
-    set args (echo $args | tr ' ' \n)
+    set -l args ''
+    if test (count $argv) -gt 1
+        set args $argv[2..-1]
+    end
     set args (getopt -s sh $fmt $args)
     fish -c "for arg in $args; echo \$arg; end"
 end
 
-function opts -d 'Helper to parse options into $flags and $rest'
+function opts -d 'Helper to parse options into $flags and $argv'
     set -l fmt $argv[1]
-    set -l args $argv[2..-1]
+    set -l args ''
+    if test (count $argv) -gt 1
+        set args $argv[2..-1]
+    end
     set -l opts (opts_getopts $fmt $args)
     set -l sep (opts_find_sep $opts)
-    set -g flags (opts_flags $sep $opts)
-    set -g rest (opts_rest $sep $opts)
+    set -g flags ''
+    set -g rest ''
+    if test $sep != 1
+        set flags $opts[1..(math $sep-1)]
+    end
+    if test $sep != (count $opts)
+        set rest $opts[(math $sep+1)..-1]
+    end
 end
