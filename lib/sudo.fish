e09dcd6de178898c6f61bc29b547ab470e44f82d
scott-linder <scott.b.linder@wmich.edu>
2015-11-02 14:48:02 -0500

    Finish removing local sudoers
    These were incompatible with the global design of the filesystem, as
    anyone could access the any channel from any other channel

--- lib/sudo.fish
+++ lib/sudo.fish
@@ -1,8 +1,5 @@
 function sudoer -d 'Check if user is admin'
-    # check global moderator list
     grep -Fxq $NICK etc/sudoers
-    # then per chan list
-    or grep -Fxq $NICK etc/sudoers.d/$CHAN ^/dev/null
     or begin
         echo "Aquaman says he doesn't like you."
         false
