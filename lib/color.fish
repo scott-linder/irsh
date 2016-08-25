b05c5249bb926029993d750e60171f19cd80477e
scott-linder <scott.b.linder@wmich.edu>
2016-08-25 12:20:58 -0400

    Update lib/color.fish to better color words
    Restrict the number of colors, as there are usually too few
    words to complete a fuller rainbow

--- lib/color.fish
+++ lib/color.fish
@@ -11,6 +11,7 @@ function random_color
 end
 
 set RAINBOW_COLORS '05' '04' '07' '08' '03' '09' '10' '11' '02' '12' '06' '13'
+set RAINBOW_WORDS_COLORS '04' '08' '09' '11' '12' '13'
 
 function rainbow
     set -l i 0
@@ -25,8 +26,8 @@ end
 function rainbow_words
     set -l i 0
     for word in (echo $argv[1] | lib/words)
-        set i (math $i%(count $RAINBOW_COLORS)+1)
-        color $RAINBOW_COLORS[$i]
+        set i (math $i%(count $RAINBOW_WORDS_COLORS)+1)
+        color $RAINBOW_WORDS_COLORS[$i]
         echo -n $word' '
     end
     color
