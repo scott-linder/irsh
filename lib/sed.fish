70cf983a2b8576b666b5f6fcd82caf291b38a25d
scott-linder <scott.b.linder@wmich.edu>
2017-03-16 18:13:42 -0400

    Add bin/fact and associated filter
    Support for 'factoids', which are filtered line replacements based on
    sed. All users can do literal replacements on full lines, and sudoers
    can exploit the full power of sed. At most one matching factoid is
    selecting per line of input, and if multiple match one is chosen at
    random.

--- lib/sed.fish
+++ lib/sed.fish
@@ -5,3 +5,7 @@ end
 function sed_escape_replacement -d 'Escape replacement argument to sed s command (assuming / as separator)'
     echo $argv | sed 's#[\\/&]#\\\\&#g'
 end
+
+function sed_escape_separator -d 'Escape only the separator to a sed command component (assuming / as a separator)'
+    echo $argv | sed 's#/#\\\/#'
+end
